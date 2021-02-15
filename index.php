5041106144726d71e596aa557c0c1e7f0aa6f6ec
Thandile001 <79053895+Thandile001@users.noreply.github.com>
2021-02-15 17:38:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <?php
-  
-// Read the variables sent via POST from our API
+// Read the variables sent via POST from AT gateway
 $sessionId   = $_POST["sessionId"];
 $serviceCode = $_POST["serviceCode"];
 $phoneNumber = $_POST["phoneNumber"];
-$text        = $_POST["text"];
+$text = $_POST["text"];
 
 if ($text == "") {
     // This is the first request. Note how we start the response with CON
     $response  = "CON What would you want to check \n";
-    $response .= "1. My Account \n";
+    $response .= "1. My Account Number \n";
     $response .= "2. My phone number";
 
 } else if ($text == "1") {
@@ -32,10 +31,10 @@ if ($text == "") {
 
 } else if ( $text == "1*2" ) {
     // This is a second level response where the user selected 1 in the first instance
-    $balance  = "KES 10,000";
+    $accountBalance  = "ZAR 10,000";
 
     // This is a terminal request. Note how we start the response with END
-    $response = "END Your balance is ".$balance;
+    $response = "END Your balance is ".$accountBalance;
 }
 
 // Echo the response back to the API
