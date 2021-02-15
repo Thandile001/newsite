a21e04ddb6b78900f53756f22fa6d115db12fe04
Thandile001 <79053895+Thandile001@users.noreply.github.com>
2021-02-15 20:51:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $text = $_POST["text"];
 
 if ($text == "") {
     // This is the first request. Note how we start the response with CON
-    $response  = "CON Welcome Mr Thandile Jwambi. Would you like to port your mobile number? \n";
+    $response  = "CON Welcome Mr Thandile Jwambi, to Loxin Worx porting services. Would you like to port your mobile number? \n";
     $response .= "1. Yes \n";
     $response .= "2. No";
 
